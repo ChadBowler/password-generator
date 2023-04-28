ab59461200fbac7133b0e2bf903dfbf0660d1286
Chad Bowler <chadbowler@gmail.com>
2023-04-28 15:45:23 -0600

    added function to generate password, and updated README.md file

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# password-generator
+# Password Generator
+
+A password generator that will prompt you for specifics about how you want your password to be generated.
+
+## Getting Started
+
+* Simply click the red "Generate Password" button, and follow the prompts.
+
+### Usage
+
+* Once you've clicked the "Generate Password" button, you will see 5 prompts:
+* The first will ask you to enter a number for the length of your password.
+* Don't worry if you enter something invalid into the prompt; you will be notified, and you'll need to start over by pressing the red button again.
+* The next four prompts will ask you to confirm which types of characters you want to include in the generation of your password. 
+* Again, if you don't pick any character types, you will be notified and will have to start over.
+* Once you've gone through all of the prompts, a password will be generated, and displayed in the box on the screen.
+
+## License
+
+* MIT License
+
+## Contact
+
+* If you have any questions, please feel free to email me: cbowlerdev@gmail.com
---
+++ assets/script/script.js
@@ -0,0 +1,70 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+
+//   if(typeof password == undefined){
+//     password = "";
+//   };
+
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  
+};
+
+function generatePassword() {
+  var password = "";
+  var choices = [];
+  const characters = {
+    upper:"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lower:"abcdefghijklmnopqrstuvwxyz",
+    numeric: "0123456789",
+    special: "!@#$%^&*()-_+={}[]|\\;:'\",.<>/?"
+  };
+
+  var passwordLength = window.prompt("How long do you want your password? (between 8 and 128 characters)");
+
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength) || (passwordLength % 1 !== 0)) {
+    alert("Password length must be a whole number between 8 and 128.");
+    // password = "";
+    return password;
+  };
+
+  var lowers = confirm("Do you want lowercase letters included in your password?");
+  var uppers = confirm("Do you want uppercase letters included in your password?");
+  var numerics = confirm("Do you want numbers included in your password?");
+  var specials = confirm("Do you want special characters included in your password?");
+  
+  if(lowers == false && uppers == false && numerics == false && specials == false) {
+    alert("Well, you need SOMETHING in your password.");
+    // password = "";
+    return password;
+  }
+
+  if(lowers) {
+    choices += characters.lower;
+  };
+
+  if(uppers) {
+    choices += characters.upper;
+  };
+
+  if(numerics) {
+    choices += characters.numeric;
+  };
+
+  if(specials) {
+    choices += characters.special;
+  };
+  
+  for(var i=0; i<passwordLength; i++){
+    var character = choices[Math.floor(Math.random()*choices.length)];
+    password += character;
+  };
+  return password;
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
